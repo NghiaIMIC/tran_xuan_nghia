282c325898db4aee120408f063182600fd012e2d
NghiaIMIC <tranxuannghia01012001@gmail.com>
2025-03-09 09:56:07 +0700

    bai_tap_phep_tinh

--- tran_xuan_nghia/mian.c
+++ tran_xuan_nghia/mian.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+
+int cong(int a, int b);
+int tru(int a, int b);
+int nhan(int a, int b);
+float chia(int a, int b);
 void main()
 {
-	printf("Hello World\n");
+	int a = 0;
+	int b = 0;
+	printf("nhap vao 2 so a va b\n");
+	scanf_s("%d %d", &a, &b);
+	printf("a + b = %d\n", cong(a, b));
+	printf("a - b = %d\n", tru(a, b));
+	printf("a * b = %d\n", nhan(a, b));
+	printf("a / b = %.2f\n", chia(a, b));
+
 }
---
+++ tran_xuan_nghia/toan.c
@@ -0,0 +1,27 @@
+int cong(int a, int b)
+{
+	int phep_cong = 0;
+	phep_cong = a + b;
+	return phep_cong;
+}
+
+int tru(int a, int b)
+{
+	int phep_tru = 0;
+	phep_tru = a - b;
+	return phep_tru;
+}
+
+int nhan(int a, int b)
+{
+	int phep_nhan = 0;
+	phep_nhan = a * b;
+	return phep_nhan;
+}
+
+float chia(int a, int b)
+{
+	float phep_chia = 0;
+	phep_chia = (float)a / b;
+	return phep_chia;
+}
