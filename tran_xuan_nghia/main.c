b8993401e9240890db05d942b0bfc4c28f2d9221
NghiaIMIC <tranxuannghia01012001@gmail.com>
2025-03-11 16:57:06 +0700

    bai tap 1 va 2

--- tran_xuan_nghia/main.c
+++ tran_xuan_nghia/main.c
@@ -1,104 +1,38 @@
 ﻿#include <stdio.h>
+#include <math.h>
 
-void main()
+void main() 
 {
-	// Nhập vào số tiền là bội số của 50000, in ra số tờ tiền và mệnh giá của nó
-	/*long money = 0;
-	int count = 0;
-	int step = 1;
-	printf("Nhap vao so tien la boi cua 50000: ");
-	scanf_s("%ld", &money);
-	while (money % 50000 != 0)
+	// Chương trình tính tổng các phân tử trong mảng
+	float Array[100];
+	printf("Nhap so phan tu cua mang\n");
+	int size = 0;
+	scanf_s("%d", &size);
+	printf("Nhap cac phan tu vao mang\n");
+	for (int i = 0; i < size; i++)
 	{
-		printf("Vui long nhap lai so tien la x50000: ");
-		scanf_s("%ld", &money);
+		scanf_s("%f", &Array[i]);
 	}
-	while (money > 0)
-	{
-		switch (step)
-		{
-		case 1:
-			while (money > 1000000)
-			{
-				money -= 500000;
-				count++;
-			}
-			printf("%d x 500000\n", count);
-			count = 0;
-			step++;
-			break;
-		case 2:
-			while (money > 500000)
-			{
-				money -= 200000;
-				count++;
-			}
-			printf("%d x 200000\n", count);
-			count = 0;
-			step++;
-			break;
-		case 3:
-			while (money > 100000)
-			{
-				money -= 100000;
-				count++;
-			}
-			printf("%d x 100000\n", count);
-			count = 0;
-			step++;
-			break;
-		case 4:
-			while (money > 0)
-			{
-				money -= 50000;
-				count++;
-			}
-			printf("%d x 50000\n", count);
-			count = 0;
-			step = 1;
-			break;
-		}
-	}*/
+	printf("\nMang vua nhap la:\n");
+	for (int i = 0; i < size; i++)
+		printf("%.2f ", Array[i]);
+	float sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += Array[i];
+	printf("\nTong cac phan tu cua mang: sum = %.2f\n", sum);
 
-	// Chương trình in ra cách đọc số bằng chữ
-	long n = 0;
-	int m = 0;
-	int temp = 1;
+	// chương trình tính trung bình các số lẻ trong mảng
 	int count = 0;
-	char* Array[] = { "linh", "muoi", "tram", "nghin", "muoi", "tram", "trieu", "muoi", "tram", "ty" };
-	char* B[] = { "khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin" };
-	printf("Nhap vao so tu nhien n > 0: ");
-	scanf_s("%ld", &n);
-	while (n > 0)
+	sum = 0;
+	for (int i = 0; i < size; i++)
 	{
-		if (n < 10)
+		if (fmod(Array[i], 2.0) != 0)
 		{
-			printf("%s ", B[n]);
-			break;
+			sum += Array[i];
+			count++;
 		}
-		else if (n >= 10 && n < 20)
-		{
-			m = n % 10;
-			printf("%s %s ", Array[1], B[m]);
-			break;
-		}
-		else
-		{
-			while (n / temp > 9)
-			{
-				m = n / temp;
-				temp *= 10;
-				count++;
-			}
-			m = n / temp;
-			n -= m * temp;
-			if (B[m] == "mot" && Array[count] == "muoi")
-				printf("%s ", Array[count]);
-			else
-				printf("%s %s ", B[m], Array[count]);
-			temp = 1;
-			count = 0;
-		}
-
 	}
+	float averageOdd = sum / count;
+	printf("Trung binh cuua mang: average = %.2f / %d = %.2f\n",sum, count, averageOdd);
+
 }
