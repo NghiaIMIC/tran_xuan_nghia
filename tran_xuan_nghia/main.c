fd918a2e3275f1101c156e0412e800acbb7e7b56
NghiaIMIC <tranxuannghia01012001@gmail.com>
2025-03-11 19:32:03 +0700

    bai tap cau 6 den 9

--- tran_xuan_nghia/main.c
+++ tran_xuan_nghia/main.c
@@ -2,108 +2,89 @@
 #include <math.h>
 /*Truyền vào hàm một địa chỉ mảng vào kích thước của mảng
 sau đó nhập vào từng phần tử mảng*/
-void nhap_mang(float Array[], int size)
+void nhap_mang(int Array[], int size)
 {
 	printf("Nhap cac phan tu vao mang\n");
 	for (int i = 0; i < size; i++)
 	{
-		scanf_s("%f", &Array[i]);
+		scanf_s("%d", &Array[i]);
 	}
 }
 
 /*In ra các phần tử có trong mảng
 ứng với kích thước của mảng*/
-void xuat_mang(float Array[], int size)
+void xuat_mang(int Array[], int size)
 {
 	for (int i = 0; i < size; i++)
-		printf("%.2f ", Array[i]);
+		printf("%d ", Array[i]);
 	printf("\n");
 }
- 
-// câu 1: Tính tong các phần tử trong mảng
-/*Truyền vào một địa chỉ mảng và kích thước của mảng
-hàm trả về kết quả tổng của mảng*/
-float tong_mang(float Array[], int size)
-{
-	float sum = 0;
-	for (int i = 0; i < size; i++)
-		sum += Array[i];
-	return sum;
-}
 
-// Câu 2: Tính trung bình các phần tửu lẻ trong mảng
-/*Truyền vào một địa chỉ mảng và kích thước của mảng
-hàm trả về kết quả trung bình cộng của các phần tử lẻ có trong mảng*/
-float avr_odd(float Array[], int size)
+// Câu 6: Viết hàm in ra số nguyên tố trong mảng
+/*Nhập vào một số nguyên bất kì
+in ra màn hình nếu số đó là số nguyên tố*/
+void soNguyenTo(int n)
 {
 	int count = 0;
-	float sum = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (fmod(Array[i], 2.0) != 0)
-		{
-			sum += Array[i];
+	for (int i = 1; i <= n; i++)
+		if (n % i == 0)
 			count++;
-		}
-	}
-	float averageOdd = sum / count;
-	return averageOdd;
+	if (count == 2)
+		printf("%d ", n);
 }
 
-// Câu 3: In ra vị trí số lớn nhất trong mảng
-/*Truyền vào một địa chỉ mảng và kích thước của mảng
-hàm trả về kết quả vị trí của số lớn nhất trong mảng*/
-int index(float Array[], int size)
+// Câu 7: Viết hàm in ra số lớn nhất trong mảng
+/*Đưa vào hàm một địa chỉ mảng và kích thước mảng
+Hàm trả về giá trị lớn nhất trong mảng*/
+int maxValue(int Array[], int size)
 {
-	float max = Array[0];
-	int index = 0;
+	int max = Array[0];
 	for (int i = 0; i < size; i++)
-	{
 		if (Array[i] >= max)
-			index = i;
-	}
-	return index;
+			max = Array[i];
+	return max;
 }
 
-// Câu 4: Thay thế các số âm trong mảng bằng số 0
-/*Truyền vào một địa chỉ mảng và kích thước của mảng
-hàm thay thế các phần tử âm bằng 0 trong mảng nếu có*/
-void thaySoAmCuamang(float Array[], int size)
+// Câu 8: Viết hàm hoán đổi 2 số nguyên
+/*Đưa vào hàm 2 số nguyên a và b
+hoán đổi giá trị của a và b*/
+void hoanDoi(int *a, int *b)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+// Câu 9: Viết hàm sắp xếp mảng theo thứ tự tăng dần
+/*Đưa vào hàm địa chỉ mảng và kích thước của mảng
+In ra mảng đã sắp xếp theo thứ tự tăng dần*/
+void rangeArray(int Array[], int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		if (Array[i] < 0)
-			Array[i] = 0;
-	}
-	xuat_mang(Array, size);
-}
 
-// Câu 5: Viết chương trình in ra các giá trị lẻ trong mảng
-void oddElement(float Array[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		if (fmod(Array[i], 2.0) == 1)
-			printf("%.2f ", Array[i]);
 	}
+
 }
 
 void main() 
 {
-	float A[100];
+	int A[100];
 	int size = 0;
+	int a = 7;
+	int b = 9;
 	printf("Nhap so phan tu cua mang\n");
 	scanf_s("%d", &size);
 	nhap_mang(A, size);
-	printf("\nMang vua nhap la:\n");
+	printf("Mang vua nhap:\n");
 	xuat_mang(A, size);
-	printf("Cau 1: Tong mang: sum = %.2f\n",tong_mang(A, size));
-	printf("Cau 2: Trung binh cong cac phan tu le trong mang: avr = %.2f\n",avr_odd(A, size));
-	printf("Cau 3: Vi tri phan tu lon nhat trong mang: index = %d\n",index(A, size));
-	printf("Cau 4: Mang ban dau\n");
-	xuat_mang(A, size);
-	printf("Mang sau khi thay: \n");
-	thaySoAmCuamang(A, size);
-	printf("Cau 5: Cac gia tri le trong mang la: \n");
-	oddElement(A, size);
+	printf("Cau 6: So nguuyen to trong mang\n");
+	for (int i = 0; i < size; i++)
+		soNguyenTo(A[i]);
+	printf("\nCau 7: Gia tri lon nhat trong mang: max = %d\n", maxValue(A, size));
+	printf("Cau 8: Gia tri cua a = %d và b = %d truoc hoan doi\n", a, b);
+	hoanDoi(&a, &b);
+	printf("Gia tri cua a = %d va b = %d sau khi hoan doi\n", a, b);
+
 }
