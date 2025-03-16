8ff68ef81a9ae06ca65d2813d10b3c7a2a119230
NghiaIMIC <tranxuannghia01012001@gmail.com>
2025-03-16 09:13:40 +0700

    Dem so ky tu

--- tran_xuan_nghia/main.c
+++ tran_xuan_nghia/main.c
@@ -1,45 +1,21 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
 
-// Hàm hoán đổi vị trí
-/*Truyền vào 2 địa chỉ biến
-Hàm thực hiện hoán đổi giá trị 2 biến*/
-void swap(int* a, int* b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-// Hàm sắp xếp giá trị trong mảng từ thấp đến cao
-void arrangeArray(int* Arr, int size)
-{
-	int flag = 0;
-	while (!flag)
-	{
-		flag = 1;
-		for (int i = 0; i < size - 1; i++)
-		{
-			if (Arr[i] > Arr[i + 1])
-			{
-				swap(Arr + i, Arr + i + 1);
-				flag = 0;
-			}
-		}
-	}
-}
 
 int main()
 {
-	int A[7] = { 2,3,1,5,-1,4,2 };
-	printf("Mang truoc khi sap xep\n");
-	for (int i = 0; i < 7; i++)
-		printf("%d ", A[i]);
-	printf("\n");
-	arrangeArray(A, 7);
-	printf("Mang sau khi sap xep\n");
-	for (int i = 0; i < 7; i++)
-		printf("%d ", A[i]);
-	printf("\n");
-	return 0;
+	char str[] = "jhhjkhnnjk";
+	int flag = 0;
+	int count = 1;
+	int i = 0;
+	while (!flag)
+	{
+		flag = 1;
+		if (str[i] != 0)
+		{
+			i++;
+			count++;
+			flag = 0;
+		}
+	}
+	printf("So ky tu trong mang: %d\n", count);
 }
