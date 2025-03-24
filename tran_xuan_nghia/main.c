906cec8379b1953567aef9a17f0d9cda4cae8b40
NghiaIMIC <tranxuannghia01012001@gmail.com>
2025-03-24 14:48:36 +0700

    struct_1

--- tran_xuan_nghia/main.c
+++ tran_xuan_nghia/main.c
@@ -1,61 +1,85 @@
 ﻿#include <stdio.h>
-#include <string.h>
-#include <malloc.h>
 
-int soLuongKyTu(char* arr)
+typedef struct
 {
-	int SL = 0;
-	while (arr[SL] != 0)
-		SL++;
-	return SL;
-}
-char* TimKyTu(char* arr, char* subArr)
+	int tu;
+	int mau;
+}phan_so_t;
+
+int UocChungLonNhat(phan_so_t A)
 {
-	int sizeArr = soLuongKyTu(arr);
-	int sizeArrSub = soLuongKyTu(subArr);
-	for (int i = 0; i < sizeArr; i++)
+	if (A.tu < 0 || A.mau < 0)
 	{
-		int j;
-		for (j = 0; j < sizeArrSub; j++)
-		{
-			if (arr[i + j] != subArr[j])
-				break;
-		}
-		if (j == sizeArrSub)
-			return arr + i;
+		A.tu = A.tu * (-1);
+		A.mau = A.mau * (-1);
 	}
-	return NULL;
+	int min = A.tu;
+	int max = 0;
+	if (A.tu > A.mau)
+		min = A.mau;
+	for (int i = 1; i <= min; i++)
+	{
+		if (A.tu % i == 0 && A.mau % i == 0)
+			max = i;
+	}
+	return max;
 }
-int TrangThaiQuat(char* data)
+
+phan_so_t RutGonPhanSo(phan_so_t A)
 {
-	char* key = "\"fan\" : \"";
-	char* stt = TimKyTu(data, key);
-	stt += soLuongKyTu(key);
-	int count = 0;
-	while (stt[count] != '"')
-	{
-		count++;
-	}
-	char* status = malloc(count + 1);
-	memset(status, 0, count + 1);
-	memcpy(status, stt, count);
-	if (TimKyTu(status, "on") != 0)
-	{
-		free(status);
-		return 1;
-	}
-	else
-	{
-		free(status);
-		return 0;
-	}
+	int ucln = UocChungLonNhat(A);
+	phan_so_t pstg;
+	pstg.tu = A.tu / ucln;
+	pstg.mau = A.mau / ucln;
+	return pstg;
 }
+
+phan_so_t CongPhanSo(phan_so_t A, phan_so_t B)
+{
+	phan_so_t sum;
+	sum.tu = A.tu * B.mau + B.tu * A.mau;
+	sum.mau = A.mau * B.mau;
+	sum = RutGonPhanSo(sum);
+	return sum;
+}
+
+phan_so_t TruPhanSo(phan_so_t A, phan_so_t B)
+{
+	phan_so_t sub;
+	sub.tu = A.tu * B.mau - B.tu * A.mau;
+	sub.mau = A.mau * B.mau;
+	//sub = RutGonPhanSo(sub);
+	return sub;
+}
+
+phan_so_t NhanPhanSo(phan_so_t A, phan_so_t B)
+{
+	phan_so_t mul;
+	mul.tu = A.tu * B.tu;
+	mul.mau = A.mau * B.mau;
+	mul = RutGonPhanSo(mul);
+	return mul;
+}
+
+phan_so_t ChiaPhanSo(phan_so_t A, phan_so_t B)
+{
+	phan_so_t div;
+	div.tu = A.tu * B.mau;
+	div.mau = A.mau * B.tu;
+	div = RutGonPhanSo(div);
+	return div;
+}
+
 void main()
 {
-	char data[] = "HTTP1.1 200 OK{"\
-		"\"light\": \"on\","\
-		"\"fan\" : \"off\","\
-		"\"motor\" : \"off\"}";
-	printf("%d\n", TrangThaiQuat(data));
-
+	phan_so_t A = {.tu = 1, .mau = 2};
+	phan_so_t B = {.tu = 2, .mau = 3};
+	phan_so_t sum = CongPhanSo(A, B);
+	phan_so_t sub = TruPhanSo(A, B);
+	phan_so_t mul = NhanPhanSo(A, B);
+	phan_so_t div = ChiaPhanSo(A, B);
+	printf("%d/%d + %d/%d = %d/%d\n", A.tu, A.mau, B.tu, B.mau, sum.tu, sum.mau);
+	printf("%d/%d - %d/%d = %d/%d\n", A.tu, A.mau, B.tu, B.mau, sub.tu, sub.mau);
+	printf("%d/%d * %d/%d = %d/%d\n", A.tu, A.mau, B.tu, B.mau, mul.tu, mul.mau);
+	printf("%d/%d : %d/%d = %d/%d\n", A.tu, A.mau, B.tu, B.mau, div.tu, div.mau);
 }
